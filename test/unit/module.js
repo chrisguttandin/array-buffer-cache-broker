86c6f9c0f2112e3233e396333497a5ef3f6ebe66
Christoph Guttandin <chrisguttandin@media-codings.com>
2018-07-31 21:23:53 +0200

    style: apply linting rules

--- test/unit/module.js
+++ test/unit/module.js
@@ -19,8 +19,8 @@ describe('module', () => {
 
             return class ExtendedWorker extends OriginalWorker {
 
-                constructor (url) {
-                    super(url);
+                constructor (rl) {
+                    super(rl);
 
                     const addEventListener = this.addEventListener;
 
